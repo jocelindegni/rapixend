5b1fbf7280db1700d0b746d39e7338f10e9c1a8c
jocelin <jocelindegni@gmail.com>
2021-05-23 15:20:57 +0200

    Spring application sekeleton

---
+++ src/test/java/com/saankaa/rapidxend/RapidxendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saankaa.rapidxend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidxendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
